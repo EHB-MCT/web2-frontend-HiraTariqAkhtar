d1361c4de6c32f766bee9f1447f0b61522b8430f
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-16 11:49:32 +0100

    for loop to get all pages of dramas

--- js/drama.js
+++ js/drama.js
@@ -1,19 +1,26 @@
 "use strict";
 
 
-async function fetchSerial (){
-    let response = await fetch("http://api.themoviedb.org/3/discover/tv?api_key=daf4ffe88f06bba73b59069934fc3b37&with_original_language=ur&sort_by=original_title.asc&include_adult=false")
+function loopSerial(){
+for(let i = 1; i<= 13; i++){
+    fetchSerial(i)
+}
+};
+
+async function fetchSerial (page){
+    let response = await fetch(`http://api.themoviedb.org/3/discover/tv?api_key=daf4ffe88f06bba73b59069934fc3b37&with_original_language=ur&sort_by=original_title.asc&include_adult=false&page=${page}`)
     return await response.json()
     .then (data => {
-        console.log(data);
-        let details = data.results
-        let filtered = details.filter(pic => pic.poster_path != null && pic.first_air_date != "" && pic.overview != "")
+        //console.log(data);
+        let results = data.results
+        let filtered = results.filter(details => details.poster_path != null && details.first_air_date != "" && details.overview != "")
         filtered.forEach(serial =>{
             let html = `<div class="drama" id="${serial.id}">
             <h2 class="title"> ${serial.name}</h2>
             <p> <b>First episode :</b> ${serial.first_air_date} </p>
-            <a href =""> <img src="https://image.tmdb.org/t/p/w200${serial.poster_path}"></a>
+            <a href ="details.html"> <img src="https://image.tmdb.org/t/p/w200${serial.poster_path}"></a>
             </div>`
+            
             let content = document.getElementById("serialData")
             content.insertAdjacentHTML("beforeend", html)
         })
@@ -26,10 +33,10 @@ async function fetchActor (){
     let response = await fetch("https://api.themoviedb.org/3/person/2058151?api_key=daf4ffe88f06bba73b59069934fc3b37")
     return await response.json()
     .then (data => {
-        console.log(data)
+       // console.log(data)
         let string = `<div class="actor" id="${data.id}">
         <h1 class="title"> ${data.name}</h1>
-        <div class ="pic_detail">
+        <div class ="flex">
         <img src="https://image.tmdb.org/t/p/w300${data.profile_path}">
        <div class="details"> 
        <h4> Date of Birth: </h4>
@@ -44,7 +51,10 @@ async function fetchActor (){
         let content = document.getElementById("actorData")
         content.insertAdjacentHTML("beforeend", string)
         })
-    let actorDetail = await fetch("https://api.themoviedb.org/3/person/{person_id}/tv_credits?api_key=<<api_key>>&language=en-US")
 }
-fetchSerial()
+
+
+
+
+loopSerial()
 fetchActor()
