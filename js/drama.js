c6c2f0d7862661cc40bd8243cfc9770c71c3edeb
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-12-19 17:03:59 +0100

    Fetch drama details and cast

--- js/drama.js
+++ js/drama.js
@@ -29,9 +29,9 @@ async function fetchSerial (page){
 }
 
 
-async function fetchActor (){
+async function fetchActor (id){
     // Iqra Aziz
-    let response = await fetch("https://api.themoviedb.org/3/person/2058151?api_key=daf4ffe88f06bba73b59069934fc3b37")
+    let response = await fetch(`https://api.themoviedb.org/3/person/${id}?api_key=daf4ffe88f06bba73b59069934fc3b37`)
     return await response.json()
     .then (data => {
        // console.log(data)
@@ -64,7 +64,7 @@ async function fetchFilmography(id){
         //console.log(data);
         let filmography = data.cast
         console.log(filmography);
-        let filtered = filmography.filter(drama => drama.poster_path != null && drama.first_air_date != "")
+        let filtered = filmography.filter(drama => drama.poster_path != null && drama.first_air_date != "" && drama.character != "")
         filtered.forEach(drama => {
             let info = `
             <div class="drama" id="${drama.id}">
@@ -90,15 +90,49 @@ async function fetchDramaDetails(id){
         console.log(data);
         let details = `<div class="drama" id="${data.id}">
         <h1 class="title"> ${data.name}</h1>
-        <p> <b>First episode :</b> ${data.first_air_date} </p>
-        <p> <b>Network :</b> ${data.networks[0].name} </p>
+        <div class ="flex">
+        <img src="https://image.tmdb.org/t/p/w300${data.poster_path}">
+       <div class="details"> 
+       <p> <b>First episode :</b> ${data.first_air_date} </p>
+       <p> <b>Last episode :</b> ${data.last_air_date} </p>
+       <p> <b>Status :</b> ${data.status}</p>
+       <p> <b>Production company :</b> ${data.production_companies[0].name} </p>
+       <p> <b>Network :</b> <a href ="${data.homepage}">${data.networks[0].name}</a> </p>
+       <p> <b>Number of episodes :</b> ${data.number_of_episodes}</p>
+       <p> <b>About :</b> ${data.overview} </p>
+        </div>
+        </div>
         </div>`
         let content = document.getElementById("detailData")
         content.insertAdjacentHTML("beforeend", details)
+        let title =`<h1 class = "title"> Cast: </h1>`
+        content.insertAdjacentHTML("beforeend", title)
+        fetchCast(data.id)
+    })
+}
+
+async function fetchCast(id){
+    let res = await fetch(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=daf4ffe88f06bba73b59069934fc3b37`)
+    return await res.json()
+    .then (data => {
+        //console.log(data);
+        let cast = data.cast
+        console.log(cast);
+        cast.forEach(actor => {
+            let info = `
+            <div class="actor" id="${actor.id}">
+            <h2 class="title"> ${actor.name}</h2>
+            <p> <b>Character name :</b> ${actor.character} </p>
+            <a href ="details.html" > <img src="https://image.tmdb.org/t/p/w200${actor.profile_path}" ></a>  
+            </div>`
+            let content = document.getElementById("cast")
+            content.insertAdjacentHTML("beforeend", info)
+        })
+        
     })
 }
 
 
 loopSerial()
-fetchActor()
-fetchDramaDetails(39907)
+fetchActor(2058151)
+fetchDramaDetails(80018)
